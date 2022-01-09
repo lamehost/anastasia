dd862c2846fbec5bbbb8ce4a89b5120571e4e332
Marco Marzetti <marco@lamehost.it>
2022-01-09 12:07:16 +0100

    Make GUI optional

--- anastasia/webapp.py
+++ anastasia/webapp.py
@@ -17,11 +17,16 @@ from anastasia.__about__ import __version__
 
 class Settings(BaseSettings):
     """App configuration schema"""
-    client_ids: str
+    # Image folder
     folder: str
+
+    # API frontend stuff
     contact_name: str
     contact_url: str
     contact_email: EmailStr
+
+    # Activate GUI
+    enable_gui: bool = False
 
     class Config:
         """Tells pydantic to import ENV from `anastasia.cfg`"""
@@ -93,32 +98,6 @@ def create_app():
         v3_0.get_api(settings.folder)
     )
 
-    # # Auth layer
-    # client_ids = [client_id.strip() for client_id in settings.client_ids.split(',')]
-    # @api.middleware("http")
-    # async def auth_layer(request: Request, call_next: Callable):
-    #     try:
-    #         key, client_id = request.headers['authorization'].split(" ", 2)
-    #         assert key == "Client-ID"
-    #     except KeyError as error:
-    #         raise HTTPException(
-    #             status_code=401,
-    #             detail="Authorization header missing"
-    #         ) from error
-    #     except (ValueError, AssertionError) as error:
-    #         raise HTTPException(
-    #             status_code=401,
-    #             detail="Authorization header malformed"
-    #         ) from error
-    #
-    #     if client_id not in client_ids:
-    #         raise HTTPException(
-    #             status_code=401,
-    #             detail="Unauthorized client id"
-    #         )
-    #
-    #     response = await call_next(request)
-    #     return response
 
 
     # Add custom headers as recommended by https://github.com/shieldfy/API-Security-Checklist#output
@@ -138,9 +117,10 @@ def create_app():
     # Add API engine to webapp
     webapp.mount('/api/', api)
 
-    # Add HTML frontend to webapp
-    app_directory = os.path.dirname(os.path.realpath(__file__))
-    static_directory = os.path.join(app_directory, 'templates')
-    webapp.mount("/", StaticFiles(directory=static_directory, html = True), name="templates")
+    if settings.enable_gui:
+        # Add HTML frontend to webapp
+        app_directory = os.path.dirname(os.path.realpath(__file__))
+        static_directory = os.path.join(app_directory, 'templates')
+        webapp.mount("/", StaticFiles(directory=static_directory, html = True), name="templates")
 
     return webapp
